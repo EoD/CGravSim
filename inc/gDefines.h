2af6d2f92f0080eadd06d93c5eb93917b0a83561
EoD <EoD@gmx.net>
2010-09-08 18:45:25 +0200

    *Codechange: cleanup of gDefines.h

--- inc/gDefines.h
+++ inc/gDefines.h
@@ -1,16 +1,17 @@
 #ifndef _INC_GRAVITY_DEFINES
 #define _INC_GRAVITY_DEFINES
 
-
+/* Debug */
 #ifdef _DEBUG
  #define DEBUG 40
 #endif
 
-
+/* Versions */
 #define BVERSION 0.9
 #define FVERSION 1.8
 #define WPTVERSION 2
 
+/* Command line arguments */
 #define FLAG_DEBUG1 "--version"
 #define FLAG_DEBUG2 "-v"
 
@@ -20,6 +21,7 @@
 #define FLAG_TIME1 "--timestep"
 #define FLAG_TIME2 "-t"
 
+/* General definitions */
 #define DATAPRECISION 20
 #define DELIMDATA ";"
 
@@ -34,8 +36,7 @@
  #define LINEDELIMNUM	1	//this comes from the fact, that unix recognizes "CR LN" as one linebreak + one character
 #endif
 
-
-//in case cmath has no isnan
+/* isnan - in case cmath has none */
 #ifndef isnan
  #include <cmath>
  #ifndef isnan
@@ -43,30 +44,22 @@
  #endif
 #endif
 
-//natural constants
+/* Natural constants */
 #define LIGHTSPEED 299792458.0 	//Lichtgeschwindigkeit in m/s
 #define GRAVCONST 6.673E-11		//Gravitationskonstante
 #define AE 1.495979E11 	//Astronomische Einheit (Längeneinheit) in m
 #define LY 9.461E15		//Lichtjahr in m
 #define PI 3.141592654; //TODO new PI requ
+
+/* Limits */
 #define DOUBLE_MIN DBL_MIN 
 #define LONGLONG_MAX LONG_MAX*LONG_MAX	//TODO FIX!
 #define LONGLONG_MIN -LONGLONG_MAX		//TODO FIX!
 
+/* Accuracy of calculation */
 #define LACCURACY 1.0E2
-/* #define TIMESTEPX 60.0*60.0			//(in s)
- #define TIMECOUNTX TIMESTEPX*10.0 //2months
- // TODO manuelle Eingabe!
- #define DATACOUNTX 365.0*24.0*60.0*60.0	//(int)(1.0*60.0/TIMESTEP);	//F�r wie lange er berechnen soll =10s
- //static final double EXACTSTEP = TIMESTEP/Math.powx(10.0, 3.0); //default exactstep 10ms
- #define LACCURACY 1.0E2 //Math.powx(10.0,3.0);	//Genauigkeit der longs: 0=m;2=cm;3=mm;6=nm;9=pm (yes it is 10^+3)
- #define SMASSCONST 21			//constant for mass-slider (nach belieben �nderbar)
- #define SRADIUSCONST 8.4			//constant for radius-slider (nach belieben �nderbar)
- #define SSPEEDCONST 8.0		//constant for speed-slider
- //static final double ZOOM = AE / (1.0E6) //the zoom of the main window
- #define RACCURACY Math.powx(10.0, 3.0) //Genauigkeit des radius-feldes (std: km=1000)
- */
 
+/* Error handling */
 #define NOERROR 0
 
 #define ERROR_UNKNOWN	-1
@@ -81,12 +74,7 @@
 #define ERROR_CALC_LIMIT_DOUBLE	4
 #define ERROR_CALC_NAN		5
 
-//Nur um sicher zu gehen das alle anderen sourcedateien die globalen variablen der main ebenfalls haben
-/*#ifndef MAIN
-	extern int error;
-	extern bool flagcalc;
-#endif*/
-
+/* OpenMP support */
 #ifdef _OPENMP
  #define FORKILLER continue
 #else
